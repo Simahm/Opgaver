9c4b26033a550c83db7deef07bb0b0f1b4edf3db
simon <admin@TA-8211-04>
2016-11-29 15:07:15 +0100

    Arv tilføjet

---
+++ Opgaver/07Arv/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Arv
+{
+    public class Person
+    {
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public string FuldtNavn()
+        {
+            return $"{this.Fornavn} {this.Efternavn}";
+        }
+    }
+}
---
+++ Opgaver/07Arv/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Arv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person() { Fornavn = "Simon", Efternavn = "Ahm Madsen" };
+            UdskrivPerson(p);
+
+            Elev e = new Elev() { Fornavn = "Hugo", Efternavn = "Ahm Bjerre", Klasselokale = "Brumbasserne" };
+            UdskrivPerson(e, "", $" - {e.Klasselokale}");
+
+            Instruktør i = new Instruktør() { Fornavn = "Doctor", Efternavn = "Strange", NøgleId = 1000 };
+            UdskrivPerson(i, "Instruktør: ", $" - NøgleId: {i.NøgleId}");
+
+            Console.ReadKey();
+        }
+
+        static void UdskrivPerson(Person x, string prefix = "", string suffix = "")
+        {
+            Console.WriteLine(prefix + x.FuldtNavn() + suffix);
+        }
+    }
+}
