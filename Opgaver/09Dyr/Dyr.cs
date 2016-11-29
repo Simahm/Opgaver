6cf8073acef6c2648b16377741643768b9b64c8b
simon <admin@TA-8211-04>
2016-11-29 13:40:51 +0100

    Sjove dyr

---
+++ Opgaver/09Dyr/Dyr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09Dyr
+{
+    public class Dyr
+    {
+        public string Navn { get; set; }
+
+        public virtual void SigNoget()
+        {
+            Console.WriteLine("Jeg er et dyr og hedder ");
+        }
+
+        static Dyr TilfældigtDyr()
+        {
+            //return System.IO.File;
+        }
+    }
+}
---
+++ Opgaver/09Dyr/Kat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09Dyr
+{
+    class Kat : Dyr
+    {
+        public override void SigNoget()
+        {
+            Console.WriteLine("Jeg er en kat og hedder ");
+        }
+    }
+}
