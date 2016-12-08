b80dce5b5dd876047cb7bf779c6e82ff0074c5fd
Simon <admin@TA-828-10>
2016-12-08 12:02:06 +0100

    Comparing færdig

---
+++ Opgaver/13Comparing/Hund.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13Comparing
+{
+    class Hund : IComparable
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Hund h = obj as Hund;
+            if (this.Alder > h.Alder)
+                return 1;
+            if (this.Alder < h.Alder)
+                return -1;          
+            return 0;
+        }
+    }
+}
---
+++ Opgaver/13Comparing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13Comparing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund[] hunde = new Hund[4];
+            hunde[0] = new Hund() { Alder = 10, Navn = "Bulder"};
+            hunde[1] = new Hund() { Alder = 5, Navn = "Lady"};
+            hunde[2] = new Hund() { Alder = 7, Navn = "Bølle"};
+            hunde[3] = new Hund() { Alder = 4, Navn = "Fido"};
+            Array.Sort(hunde);
+            foreach (var item in hunde)
+            {
+                Console.WriteLine(item.Navn + " " + item.Alder);
+            }
+            Console.ReadKey();
+        }
+    }
+}
