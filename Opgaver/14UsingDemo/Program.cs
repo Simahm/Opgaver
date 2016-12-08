d8a726127586ad5e5e5fbf6000f985e063409557
Simon <admin@TA-828-10>
2016-12-08 14:57:37 +0100

    Collections1 færdig

---
+++ Opgaver/14UsingDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14UsingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = File.OpenText("X:/Michell/dyrenavne.txt");
+            while (stream.Peek() != -1)
+            {
+                string navn = stream.ReadLine();
+                Console.WriteLine(navn);
+            }
+
+            stream.Close();
+            stream = null;
+
+            Console.WriteLine("----------------------Ny listning---------------------");
+
+            stream = File.OpenText("X:/Michell/dyrenavne.txt");
+            using (stream)
+            {
+                while (stream.Peek() != -1)
+                {
+                    string navn = stream.ReadLine();
+                    Console.WriteLine(navn);
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
---
+++ Opgaver/CollectionsDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15CollectionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> liste = new List<Person>
+            {
+                new Person() {Id = 1, Navn = "Poul" },
+                new Person() {Id = 2, Navn = "Svend" },
+                new Person() {Id = 3, Navn = "Ulla" },
+                new Person() {Id = 4, Navn = "Frode" }
+            };
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item.Navn);
+            }
+
+            Dictionary<int, Person> dict = new Dictionary<int, Person>();
+            dict.Add(1, new Person() { Navn = "Lilian" });
+            dict.Add(2, new Person() { Navn = "John" });
+            dict.Add(3, new Person() { Navn = "Orla" });
+
+            for (int i = 1; i <= dict.Values.Count; i++)
+            {
+                Console.WriteLine(dict[i].Navn);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
