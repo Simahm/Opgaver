01698b1e5f2e0e87d7f4f9a1c67b650081bd339a
Simon <admin@TA-828-10>
2016-12-08 15:34:15 +0100

    Collections færdig

---
+++ Opgaver/CollectionsDemo/Bunke.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15CollectionsDemo
+{
+    class Bunke
+    {
+        private Stack<Kort> stak = new Stack<Kort>();
+
+        public void TilføjKort(Kort k)
+        {
+            stak.Push(k);
+        }
+
+        public Kort FjernKort()
+        {
+            if (stak.Count > 0)
+                return stak.Pop();
+            return null;
+        }
+
+        public void VisKort()
+        {
+            foreach (var item in stak)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
--- Opgaver/CollectionsDemo/Program.cs
+++ Opgaver/CollectionsDemo/Program.cs
@@ -10,6 +10,50 @@ namespace _15CollectionsDemo
     {
         static void Main(string[] args)
         {
+
+            //PrintListe();
+            //PrintDict();
+
+            Bunke bunke = new Bunke();
+            bunke.TilføjKort(new Kort() {Kulør = "Spar", Værdi = 2});
+            bunke.TilføjKort(new Kort() {Kulør = "Hjerter", Værdi = 14});
+            bunke.TilføjKort(new Kort() {Kulør = "Ruder", Værdi = 3});
+
+            bunke.VisKort();
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine(bunke.FjernKort());
+            Console.WriteLine();
+            bunke.VisKort();
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine(bunke.FjernKort());
+            Console.WriteLine();
+            bunke.VisKort();
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine(bunke.FjernKort());
+            Console.WriteLine();
+            bunke.VisKort();
+
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine(bunke.FjernKort());
+            Console.WriteLine();
+            bunke.VisKort();
+
+            Console.ReadKey();
+        }
+
+        static void PrintListe()
+        {
             List<Person> liste = new List<Person>
             {
                 new Person() {Id = 1, Navn = "Poul" },
@@ -22,7 +66,10 @@ namespace _15CollectionsDemo
             {
                 Console.WriteLine(item.Navn);
             }
+        }
 
+        static void PrintDict()
+        {
             Dictionary<int, Person> dict = new Dictionary<int, Person>();
             dict.Add(1, new Person() { Navn = "Lilian" });
             dict.Add(2, new Person() { Navn = "John" });
@@ -33,7 +80,6 @@ namespace _15CollectionsDemo
                 Console.WriteLine(dict[i].Navn);
             }
 
-            Console.ReadKey();
         }
     }
 }
