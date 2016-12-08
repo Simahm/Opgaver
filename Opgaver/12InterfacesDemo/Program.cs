dca4a1d516650213a84ea33cb5236ce33b6758e2
Simon <admin@TA-828-10>
2016-12-08 10:56:09 +0100

    Interfaces færdig

---
+++ Opgaver/12InterfacesDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12InterfacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbFunktioner[] liste = new IDbFunktioner[5];
+            liste[0] = new Hund() { Navn = "Fido" };
+            liste[1] = new Hund() { Navn = "King" };
+            liste[2] = new Hund() { Navn = "Basse" };
+            liste[3] = new Ubåd() { Nummer = 1, Turbine = 2.1 };
+            liste[4] = new Ubåd() { Nummer = 2, Turbine = 2.1 };
+
+            foreach (IDbFunktioner item in liste)
+            {
+                item.Gem();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Opgaver/12InterfacesDemo/Ubåd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12InterfacesDemo
+{
+    class Ubåd : IDbFunktioner
+    {
+        public int Nummer { get; set; }
+        public double Turbine { get; set; }
+
+        public void Gem()
+        {
+            Console.WriteLine($"Gemmer ubåd med værdierne {Nummer} og {Turbine}");
+        }
+    }
+}
