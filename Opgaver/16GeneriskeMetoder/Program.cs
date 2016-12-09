43e97872aa409a26b323ca21f37f483891ccfe74
Simon <admin@TA-828-10>
2016-12-09 10:04:47 +0100

    øv. 16 og 17 færdig

---
+++ Opgaver/16GeneriskeMetoder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16GeneriskeMetoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Simon";
+            string efternavn = "Ahm";
+            Console.WriteLine(fornavn + " " + efternavn);
+            Swap<string>(ref fornavn, ref efternavn);
+            Console.WriteLine(fornavn + " " + efternavn);
+
+            int x = 10;
+            int y = 25550;
+            Console.WriteLine(x + " " + y);
+            Swap(ref x, ref y);
+            Console.WriteLine(x + " " + y);
+
+            Console.ReadKey();
+        }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
---
+++ Opgaver/17AppSetting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17AppSetting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetAppSetting("ip", "test"));
+
+            Console.WriteLine(GetAppSettingGeneric("maxrecords", 80));
+
+            Console.ReadKey();
+        }
+
+        static string GetAppSetting(string key, string defaultValue)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings[key];
+            if (value == null)
+                return defaultValue;
+
+            return value;
+        }
+
+        static T GetAppSettingGeneric<T>(string key, T defaultValue)
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings[key];
+            if (value == null)
+                return defaultValue;
+
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
